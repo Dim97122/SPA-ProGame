2213866aef47b41e73554d843984250a0cd7adb9
Dim <dimitri.kiavue@gmail.com>
2020-04-29 12:06:04 +0200

    [ADD] Webpack + Templates

---
+++ src/js/routes.js
@@ -0,0 +1,9 @@
+// import { Home } from "./Home";
+// import { PageList } from "./PageList";
+// import { PageDetails } from "./PageDetails";
+
+const routes = {
+	"": Home,
+	pagelist: PageList,
+	pagedetails: PageDetails,
+};
